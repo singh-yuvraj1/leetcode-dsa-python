292081df215b83d6da078c008bce1c0561db2ca1
singh-yuvraj1 <yuvraj.singh.95928@gmail.com>
2026-04-10 22:15:45 +0530

    Solved a problem named longest even odd subarray with threshold

---
+++ arrays/easy/longest_eo_subarray.py
@@ -0,0 +1,70 @@
+# 2760. Longest Even Odd Subarray With Threshold
+# You are given a 0-indexed integer array nums and an integer threshold.
+
+# Find the length of the longest subarray of nums starting at index l and ending at index r (0 <= l <= r < nums.length) that satisfies the following conditions:
+# nums[l] % 2 == 0
+# For all indices i in the range [l, r - 1], nums[i] % 2 != nums[i + 1] % 2
+# For all indices i in the range [l, r], nums[i] <= threshold
+# Return an integer denoting the length of the longest such subarray.
+# Note: A subarray is a contiguous non-empty sequence of elements within an array.
+
+ 
+# Example 1:
+# Input: nums = [3,2,5,4], threshold = 5
+# Output: 3
+# Explanation: In this example, we can select the subarray that starts at l = 1 and ends at r = 3 => [2,5,4]. This subarray satisfies the conditions.
+# Hence, the answer is the length of the subarray, 3. We can show that 3 is the maximum possible achievable length.
+
+
+# Example 2:
+# Input: nums = [1,2], threshold = 2
+# Output: 1
+# Explanation: In this example, we can select the subarray that starts at l = 1 and ends at r = 1 => [2]. 
+# It satisfies all the conditions and we can show that 1 is the maximum possible achievable length.
+
+
+# Example 3:
+# Input: nums = [2,3,4,5], threshold = 4
+# Output: 3
+# Explanation: In this example, we can select the subarray that starts at l = 0 and ends at r = 2 => [2,3,4]. 
+# It satisfies all the conditions.
+# Hence, the answer is the length of the subarray, 3. We can show that 3 is the maximum possible achievable length.
+ 
+
+# Constraints:
+# 1 <= nums.length <= 100 
+# 1 <= nums[i] <= 100 
+# 1 <= threshold <= 100
+
+
+class Solutionn(object):
+    def longestAlternatingSubarray(self, nums, threshold):
+        n = len(nums)
+        max_len = 0
+        
+        for i in range(n):
+            if nums[i] % 2 == 0 and nums[i] <= threshold:
+                length = 1
+                
+                for j in range(i+1, n):
+                    if nums[j] <= threshold and nums[j] % 2 != nums[j-1] % 2:
+                        length += 1
+                    else:
+                        break
+                
+                max_len = max(max_len, length)
+        
+        return max_len
+if __name__== "__main__":
+    a = Solutionn()
+    nums1 = [3,2,5,4]
+    threshold1 = 5
+    print(a.longestAlternatingSubarray(nums1, threshold1))  # 3
+        
+    nums2 = [1,2]
+    threshold2 = 2
+    print(a.longestAlternatingSubarray(nums2, threshold2))  # 1
+        
+    nums3 = [2,3,4,5]
+    threshold3 = 4
+    print(a.longestAlternatingSubarray(nums3, threshold3))  # 3 
