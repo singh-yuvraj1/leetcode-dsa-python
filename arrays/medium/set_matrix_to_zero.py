fd99b8837c29385e1c3745e28520188fe0e068ff
singh-yuvraj1 <yuvraj.singh.95928@gmail.com>
2026-04-29 23:00:00 +0530

    Set matrix to zero with optimal solution

--- arrays/medium/set_matrix_to_zero.py
+++ arrays/medium/set_matrix_to_zero.py
@@ -57,3 +57,53 @@ if __name__== "__main__":
 
 ## Time Complexity: O(mn) where m is the number of rows and n is the number of columns in the input matrix. We have two nested loops that iterate through the matrix to find the zero elements and set the corresponding rows and columns to zero.
 ## Space Complexity: O(m + n) since we are using two additional lists to store the indices of the rows and columns that need to be set to zero. In the worst case, if all elements in the matrix are zero, we would need to store all row and column indices, resulting in O(m + n) space complexity.
+
+
+
+#----------------------------------Optimal Solution----------------------------------
+class Solution1(object):
+    def setZeroes(self, matrix):
+        rows = len(matrix)
+        cols = len(matrix[0])
+        first_row_zero = False
+        first_col_zero = False
+        
+        for i in range(rows):
+            if matrix[i][0] == 0:
+                first_col_zero = True
+                break
+        
+        for j in range(cols):
+            if matrix[0][j] == 0:
+                first_row_zero = True
+                break
+        
+        for i in range(1, rows):
+            for j in range(1, cols):
+                if matrix[i][j] == 0:
+                    matrix[i][0] = 0
+                    matrix[0][j] = 0
+        
+        for i in range(1, rows):
+            for j in range(1, cols):
+                if matrix[i][0] == 0 or matrix[0][j] == 0:
+                    matrix[i][j] = 0
+        
+        if first_col_zero:
+            for i in range(rows):
+                matrix[i][0] = 0
+        
+        if first_row_zero:
+            for j in range(cols):
+                matrix[0][j] = 0
+if __name__== "__main__":
+    a = Solution1()
+    matrix1 = [[1,1,1],[1,0,1],[1,1,1]]
+    a.setZeroes(matrix1)
+    print(matrix1)  # [[1,0,1],[0,0,0],[1,0,1]]
+
+    matrix2 = [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
+    a.setZeroes(matrix2)
+    print(matrix2)  # [[0,0,0,0],[0,4,5,0],[0,3,1,0]]
+## Time Complexity: O(mn) where m is the number of rows and n is the number of columns in the input matrix. We have two nested loops that iterate through the matrix to find the zero elements and set the corresponding rows and columns to zero.
+## Space Complexity: O(1) since we are using only a constant amount of extra space to store the boolean variables for the first row and first column. We are modifying the input matrix in place without using any additional data structures that grow with the input size.
