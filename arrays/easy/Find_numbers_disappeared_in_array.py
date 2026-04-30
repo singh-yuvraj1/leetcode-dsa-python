db34bea58a586d0e6766fe28c4f6441bfec82e07
singh-yuvraj1 <yuvraj.singh.95928@gmail.com>
2026-04-30 23:09:31 +0530

    Find disappeared numbers form array optimal solution

--- arrays/easy/Find_numbers_disappeared_in_array.py
+++ arrays/easy/Find_numbers_disappeared_in_array.py
@@ -45,3 +45,31 @@ if __name__== "__main__":
 ## Time Complexity: O(n^2) where n is the length of the input array nums. We have a loop that iterates through the numbers from 1 to n, and for each number, we check if it is present in the nums array using the "in" operator, which takes O(n) time in the worst case.
 ## Space Complexity: O(m) where m is the number of missing numbers in the range [1, n]. In the worst case, if all numbers from 1 to n are missing, we would need to store all n numbers in the missing_numbers list, resulting in O(n) space complexity.
 
+
+
+#----------------------------------Optimal Solution----------------------------------
+class Solution(object):
+    def findDisappearedNumbers(self, nums):
+        n = len(nums)
+        missing_numbers = []
+        
+        for i in range(n):
+            index = abs(nums[i]) - 1
+            if nums[index] > 0:
+                nums[index] = -nums[index]
+        
+        for i in range(n):
+            if nums[i] > 0:
+                missing_numbers.append(i + 1)
+        
+        return missing_numbers
+if __name__== "__main__":
+    a = Solution()
+    nums1 = [4,3,2,7,8,2,3,1]
+    print(a.findDisappearedNumbers(nums1))  # [5,6]
+
+    nums2 = [1,1]
+    print(a.findDisappearedNumbers(nums2))  # [2]
+
+## Time Complexity: O(n) where n is the length of the input array nums. We have two loops that iterate through the nums array, each taking O(n) time, resulting in a total time complexity of O(n).
+## Space Complexity: O(m) where m is the number of missing numbers in the range [1, n]. In the worst case, if all numbers from 1 to n are missing, we would need to store all n numbers in the missing_numbers list, resulting in O(n) space complexity. However, since we are modifying the input array nums in place and not using any additional data structures to store intermediate results, the space complexity can be considered O(1) if we do not count the output list as extra space.
