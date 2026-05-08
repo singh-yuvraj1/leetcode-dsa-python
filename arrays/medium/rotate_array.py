c9c8b3eb6db9b64429309ceb8b2bd08106b54cf8
singh-yuvraj1 <yuvraj.singh.95928@gmail.com>
2026-05-08 23:34:44 +0530

    solved rotate array using reverse method

--- arrays/medium/rotate_array.py
+++ arrays/medium/rotate_array.py
@@ -60,3 +60,35 @@ if __name__== "__main__":
 
 
 
+#Solution 2: Using reverse method
+class Solution(object):
+    def rotate(self, nums, k):
+        n = len(nums)
+        k = k % n  # Handle cases where k is greater than n
+        
+        # Reverse the entire array
+        self.reverse(nums, 0, n - 1)
+        # Reverse the first k elements
+        self.reverse(nums, 0, k - 1)
+        # Reverse the remaining n-k elements
+        self.reverse(nums, k, n - 1)
+    
+    def reverse(self, nums, start, end):
+        while start < end:
+            nums[start], nums[end] = nums[end], nums[start]
+            start += 1
+            end -= 1
+if __name__== "__main__":
+    a = Solution()
+    nums1 = [1,2,3,4,5,6,7]
+    k1 = 3
+    a.rotate(nums1, k1)
+    print(nums1)  # Output: [5,6,7,1,2,3,4]
+
+    nums2 = [-1,-100,3,99]
+    k2 = 2
+    a.rotate(nums2, k2)
+    print(nums2)  # Output: [3,99,-1,-100]
+#time complexity: O(n), where n is the length of the input array nums. We reverse the entire array and then reverse two subarrays, each of which takes O(n) time in total.
+#space complexity: O(1), as we are performing the rotations in-place without using any additional data structures.
+
