9627f4f0cc874d5c5ae4f1a629e23df60e070219
singh-yuvraj1 <yuvraj.singh.95928@gmail.com>
2026-05-21 22:40:46 +0530

    find length of longest common prefix using brute force

---
+++ arrays/medium/length_of_longest_common_prefix.py
@@ -0,0 +1,68 @@
+# "3043. Find the Length of the Longest Common Prefix
+# Solved
+# Medium
+# Topics
+# premium lock icon
+# Companies
+# Hint
+# You are given two arrays with positive integers arr1 and arr2.
+
+# A prefix of a positive integer is an integer formed by one or more of its digits, starting from its leftmost digit. For example, 123 is a prefix of the integer 12345, while 234 is not.
+
+# A common prefix of two integers a and b is an integer c, such that c is a prefix of both a and b. For example, 5655359 and 56554 have common prefixes 565 and 5655 while 1223 and 43456 do not have a common prefix.
+
+# You need to find the length of the longest common prefix between all pairs of integers (x, y) such that x belongs to arr1 and y belongs to arr2.
+
+# Return the length of the longest common prefix among all pairs. If no common prefix exists among them, return 0.
+
+ 
+
+# Example 1:
+
+# Input: arr1 = [1,10,100], arr2 = [1000]
+# Output: 3
+# Explanation: There are 3 pairs (arr1[i], arr2[j]):
+# - The longest common prefix of (1, 1000) is 1.
+# - The longest common prefix of (10, 1000) is 10.
+# - The longest common prefix of (100, 1000) is 100.
+# The longest common prefix is 100 with a length of 3.
+# Example 2:
+
+# Input: arr1 = [1,2,3], arr2 = [4,4,4]
+# Output: 0
+# Explanation: There exists no common prefix for any pair (arr1[i], arr2[j]), hence we return 0.
+# Note that common prefixes between elements of the same array do not count.
+ 
+
+# Constraints:
+
+# 1 <= arr1.length, arr2.length <= 5 * 104
+# 1 <= arr1[i], arr2[i] <= 108"
+
+#brute force solution---
+class Solution:
+    def longestCommonPrefix(self, arr1, arr2):
+        max_length = 0
+        for num1 in arr1:
+            for num2 in arr2:
+                common_prefix_length = self.common_prefix_length(num1, num2)
+                max_length = max(max_length, common_prefix_length)
+        return max_length
+
+    def common_prefix_length(self, num1, num2):
+        str_num1 = str(num1)
+        str_num2 = str(num2)
+        length = 0
+        for digit1, digit2 in zip(str_num1, str_num2):
+            if digit1 == digit2:
+                length += 1
+            else:
+                break
+        return length
+if __name__ == "__main__":
+    solution = Solution()
+    print(solution.longestCommonPrefix([1,10,100], [1000]))  # Output: 3
+    print(solution.longestCommonPrefix([1,2,3], [4,4,4]))  # Output: 0
+# Time Complexity: O(n * m * k), where n and m are the lengths of arr1 and arr2 respectively, and k is the average number of digits in the integers.
+# Space Complexity: O(1) since we are using only a constant amount of extra space
+
