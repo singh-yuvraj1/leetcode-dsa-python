abf61f846dce96470dbb15567a903c738483c8e8
singh-yuvraj1 <yuvraj.singh.95928@gmail.com>
2026-05-23 22:23:56 +0530

    solved check if array is sorted and rotated

---
+++ arrays/easy/check_array_is_sorted_and_rotated.py
@@ -0,0 +1,49 @@
+# 1752. Check if Array Is Sorted and Rotated
+# Given an array nums, return true if the array was originally sorted in non-decreasing order, then rotated some number of positions (including zero). Otherwise, return false.
+# There may be duplicates in the original array.
+
+# Note: An array A rotated by x positions results in an array B of the same length such that B[i] == A[(i+x) % A.length] for every valid index i.
+
+# Example 1:
+# Input: nums = [3,4,5,1,2]
+# Output: true
+# Explanation: [1,2,3,4,5] is the original sorted array.
+# You can rotate the array by x = 2 positions to begin on the element of value 3: [3,4,5,1,2].
+
+
+# Example 2:
+# Input: nums = [2,1,3,4]
+# Output: false
+# Explanation: There is no sorted array once rotated that can make nums.
+
+
+# Example 3:
+# Input: nums = [1,2,3]
+# Output: true
+# Explanation: [1,2,3] is the original sorted array.
+# You can rotate the array by x = 0 positions (i.e. no rotation) to make nums.
+ 
+
+# Constraints:
+# 1 <= nums.length <= 100
+# 1 <= nums[i] <= 100
+
+
+#brute force solution---
+class Solution:
+    def check(self, nums):
+        count = 0
+        for i in range(1, len(nums)):
+            if nums[i] < nums[i - 1]:
+                count += 1
+        if count == 0 or (count == 1 and nums[0] >= nums[-1]):
+            return True
+        return False
+if __name__ == "__main__":
+    solution = Solution()
+    print(solution.check([3,4,5,1,2]))  # Output: true
+    print(solution.check([2,1,3,4]))  # Output: false
+    print(solution.check([1,2,3]))  # Output: true
+# Time Complexity: O(n) since we may have to check each element in the worst case.
+# Space Complexity: O(1) since we are using only a constant amount of extra space.
+
