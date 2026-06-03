3336d8c236ad62da8a825b37d06c0a27261f9321
singh-yuvraj1 <yuvraj.singh.95928@gmail.com>
2026-06-03 22:48:37 +0530

    solved happy number using brute force

---
+++ arrays/easy/happy_number.py
@@ -0,0 +1,62 @@
+# 202. Happy Number
+# Write an algorithm to determine if a number n is happy.
+# A happy number is a number defined by the following process:
+# Starting with any positive integer, replace the number by the sum of the squares of its digits.
+# Repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1.
+# Those numbers for which this process ends in 1 are happy.
+# Return true if n is a happy number, and false if not.
+
+ 
+
+# Example 1:
+# Input: n = 19
+# Output: true
+# Explanation:
+# 12 + 92 = 82
+# 82 + 22 = 68
+# 62 + 82 = 100
+# 12 + 02 + 02 = 1
+
+
+# Example 2:
+# Input: n = 2
+# Output: false
+ 
+
+# Constraints:
+# 1 <= n <= 231 - 1
+
+
+#brute force solution
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        visited = []
+
+        while n != 1:
+            if n in visited:
+                return False
+
+            visited.append(n)
+
+            total = 0
+
+            while n > 0:
+                digit = n % 10
+                total += digit * digit
+                n //= 10
+
+            n = total
+
+        return True
+if __name__ == "__main__":
+    n = 19
+    print(Solution().isHappy(n))
+    n=2
+    print(Solution().isHappy(n))
+#output: true, false
+#time complexity: O(k²) approximately because searching in list takes O(k)
+# k = number of generated numbers
+#space complexity: O(k) because we are storing the generated numbers in the visited list.
+
+
+
