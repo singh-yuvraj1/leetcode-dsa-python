f60f20560c06a88f76641a71e141bfced665bd80
singh-yuvraj1 <yuvraj.singh.95928@gmail.com>
2026-06-12 23:50:53 +0530

    solving minimum stack using two stacks

--- arrays/medium/min_stack.py
+++ arrays/medium/min_stack.py
@@ -78,3 +78,52 @@ class MinStack(object):
 # space complexity: O(n) for the stack. 
 
 
+#optimized solution using 2 stacks
+
+class MinStack(object):
+
+    def __init__(self):
+        self.stack = []
+        self.minStack = []
+
+    def push(self, value):
+        """
+        :type value: int
+        :rtype: None
+        """
+        self.stack.append(value)
+
+        if not self.minStack or value <= self.minStack[-1]:
+            self.minStack.append(value)
+
+    def pop(self):
+        """
+        :rtype: None
+        """
+        if self.stack[-1] == self.minStack[-1]:
+            self.minStack.pop()
+
+        self.stack.pop()
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        return self.stack[-1]
+
+    def getMin(self):
+        """
+        :rtype: int
+        """
+        return self.minStack[-1]
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(value)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
+
+#time complexity: O(1) for all operations (push, pop, top, getMin)
+# space complexity: O(n) for the main stack and O(m) for the min stack, where m is the number of minimum values stored in the min stack. In the worst case, if all values are the same and are the minimum, then m can be equal to n, resulting in O(n) space complexity for both stacks.
